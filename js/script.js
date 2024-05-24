6e4998ec572a8d92bd6ceadee611e5d35eefb551
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-05-24 12:11:40 +0200

    Created an array with 4 objects
    4 objects containing Pokemon data

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+    {name: 'Pikachu', height: '0.4 m', weight: '6 kg', type: 'electric' },
+    {name: 'Kadabra', height: '1.3 m' , weight: '56.5 kg' , type: 'pschic'},
+    {name: 'Ho-oh', height: '3.8 m' , weight: '199 kg' , type: 'fire & flying' },
+    {name: 'Mewtwo' , height: '2.0 m' , weight: '122 kg' , type: 'psychic' },
+];
