a6926b6441d4ecb26ee975b6a55f88b1a358c9ca
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-05-27 17:29:01 +0200

    Created IIEF, moved the array inside and created functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-let pokemonList = [
-    { name: 'Pikachu', height: '0.4 m', weight: '6 kg', type: 'electric' },
-    { name: 'Kadabra', height: '1.3 m', weight: '56.5 kg', type: 'pschic' },
-    { name: 'Ho-oh', height: '3.8 m', weight: '199 kg', type: 'fire & flying' },
-    { name: 'Mewtwo', height: '2.0 m', weight: '122 kg', type: 'psychic' },
-];
+
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        { name: 'Pikachu', height: 0.4, weight: 6, type: 'electric' },
+        { name: 'Kadabra', height: 1.3, weight: 56.5, type: 'psychic ' },
+        { name: 'Ho-oh', height: 3.8, weight: 199, type: ['fire', 'flying'] },
+        { name: 'Mewtwo', height: 2.0, weight: 122, type: 'psychic' },
+    ];
+
+    function getAll () {
+        return pokemonList;
+    }
+
+    function add (item) {
+        return pokemonList.push(item);
+    }
+
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})();
+
 
 // Loop through pokemonList array
 pokemonList.forEach((pokemon) => {
@@ -14,3 +34,4 @@ pokemonList.forEach((pokemon) => {
     } else {
         document.write(`${pokemon.name} (Height: ${pokemon.height}, Weight: ${pokemon.weight}, Type: ${pokemon.type})<br>`)
     }
+});
