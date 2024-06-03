e57de625698209663d43cbe0352643304d1db1c3
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-06-03 16:22:04 +0200

    Added Pokedex api

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let pokemonRepository = (function () {
 
     let pokemonList = [];
 
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function getAll() {
         return pokemonList;
