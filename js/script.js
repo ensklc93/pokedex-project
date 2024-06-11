8e36d06858d17010c626d383e96c5481f60b0abc
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-06-11 23:01:57 +0200

    Lint script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = []
 
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150"
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'
 
   function getAll() {
     return pokemonList
@@ -12,21 +12,21 @@ let pokemonRepository = (function () {
   }
 
   function addListItem(pokemon) {
-    let pokemonUl = document.querySelector(".list-group")
-    let listItem = document.createElement("li")
-    listItem.classList.add("list-group-item")
-    let button = document.createElement("button")
+    let pokemonUl = document.querySelector('.list-group')
+    let listItem = document.createElement('li')
+    listItem.classList.add('list-group-item')
+    let button = document.createElement('button')
     button.innerText = pokemon.name
-    button.classList.add("btn", "btn-dark")
-    button.setAttribute("type", "button")
-    button.setAttribute("data-toggle", "modal")
-    button.setAttribute("data-target", "#pokedexModal")
+    button.classList.add('btn', 'btn-dark')
+    button.setAttribute('type', 'button')
+    button.setAttribute('data-toggle', 'modal')
+    button.setAttribute('data-target', '#pokedexModal')
     listItem.appendChild(button)
     pokemonUl.appendChild(listItem)
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
       showDetails(pokemon)
-      modalBody.innerHTML = ""
-      modalTitle.innerHTML = ""
+      modalBody.innerHTML = ''
+      modalTitle.innerHTML = ''
     })
   }
 
@@ -66,26 +66,26 @@ let pokemonRepository = (function () {
       })
   }
 
-  const modalBody = document.querySelector(".modal-body")
-  const modalTitle = document.querySelector(".modal-title")
-  const buttonClose = document.querySelector(".close")
+  const modalBody = document.querySelector('.modal-body')
+  const modalTitle = document.querySelector('.modal-title')
+  const buttonClose = document.querySelector('.close')
 
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       modalTitle.textContent = item.name
-      buttonClose.textContent = "X"
+      buttonClose.textContent = 'X'
 
-      let pokemonImage = document.createElement("img")
-      pokemonImage.setAttribute("src", item.imageUrl)
-      pokemonImage.classList.add("pokemon-image")
+      let pokemonImage = document.createElement('img')
+      pokemonImage.setAttribute('src', item.imageUrl)
+      pokemonImage.classList.add('pokemon-image')
 
-      let pokemonHeight = document.createElement("p")
+      let pokemonHeight = document.createElement('p')
       pokemonHeight.textContent = `Height: ${item.height}`
-      pokemonHeight.classList.add("pokemon-height")
+      pokemonHeight.classList.add('pokemon-height')
 
-      let pokemonType = document.createElement("p")
-      pokemonType.textContent = `Types: ${item.types.map(element => element.type.name).join(", ")}`
-      pokemonType.classList.add("pokemon-type")
+      let pokemonType = document.createElement('p')
+      pokemonType.textContent = `Types: ${item.types.map((element) => element.type.name).join(', ')}`
+      pokemonType.classList.add('pokemon-type')
 
       modalBody.appendChild(pokemonImage)
       modalBody.appendChild(pokemonHeight)
@@ -109,3 +109,6 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon)
   })
 })
+
+const message = 'Hello, world'
+console.log(message)
